ecb80409b25ff31866c00fb18459e12dec05a40f
m00zy <maxwellzhu123@gmail.com>
2025-08-24 16:37:54 +1200

    fix bug where images can't be found by moving all images from src/assets/images to public/images

--- src/app.jsx
+++ src/app.jsx
@@ -4,37 +4,37 @@ import Navigation from './components/Navigation';
 export default function App() {
   const images = [
     { 
-      src: '/src/assets/images/01/img1.jpg',
+      src: '/images/01/img1.jpg',
       title: 'Hatch Coffee Table',
       description: 'A stylish and functional piece suitable for any modern living space.'
     },
     { 
-      src: 'src/assets/images/02/img1.jpg',
+      src: '/images/02/img1.jpg',
       title: 'Rook Stool',
       description: 'A versatile stool that complements a variety of interior styles.'
     },
     { 
-      src: 'src/assets/images/03/img1.jpg',
+      src: '/images/03/img1.jpg',
       title: 'Trace Table Lamp',
       description: 'A sleek lamp that provides both illumination and aesthetic appeal.'
     },
     { 
-      src: 'src/assets/images/04/img1.jpg',
+      src: '/images/04/img1.jpg',
       title: 'Lounger Chair',
       description: 'A comfortable chair designed for relaxation and style.'
     },
     {
-      src: 'src/assets/images/05/img1.jpg',
+      src: '/images/05/img1.jpg',
       title: 'Buoy Couch',
       description: 'A plush couch offering the perfect blend of comfort and elegance for any living room.'
     },
     { 
-      src: 'src/assets/images/04/img1.jpg',
+      src: '/images/04/img1.jpg',
       title: 'Lounger Chair',
       description: 'A comfortable chair designed for relaxation and style.'
     },
     {
-      src: 'src/assets/images/05/img1.jpg',
+      src: '/images/05/img1.jpg',
       title: 'Buoy Couch',
       description: 'A plush couch offering the perfect blend of comfort and elegance for any living room.'
     }
